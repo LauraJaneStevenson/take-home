84a503868fe029d39d68b497166db3cb524ba245
vagrant <vagrant@vagrant.vm>
2019-12-14 21:09:38 +0000

    added update button

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,46 +1,42 @@
 
 const completeTask = (id) => {
 
-
-
     $.post('/complete_task',{ 'id':id }, (res) => {
 
+        
         alert(res);
     });
 
 };
 
+// $('#update').on('click', () => {
+
+    $.get('/all_tasks.json',(res)=>{
+
+        for(const task of res){
+
+            if(task.complete){
+                $('ul.tasks').append(
+                    `<input class="form-check-input" 
+                    type="checkbox" 
+                    id="invalidCheck"
+                    onclick="">
+                    <li class="done" id="${task.id}">${task.task_str}</li>`
+                    );
+            }else{
+
+                $('ul.tasks').append(
+                    `<input class="form-check-input" 
+                    type="checkbox"  
+                    id="invalidCheck"
+                    onclick="completeTask(${task.id})">
+                    <li id="${task.id}">${task.task_str}</li>`
+                    );
+            }
+        };
+    });
 
 
-$.get('/all_tasks.json',(res)=>{
 
-    for(const task of res){
-
-        if(task.complete){
-            $('ul.tasks').append(
-                `<input class="form-check-input" 
-                type="checkbox" 
-                id="invalidCheck"
-                onclick="">
-                <li class='done'>${task.task_str}</li>`
-                );
-        }else{
-
-            $('ul.tasks').append(
-                `<input class="form-check-input" 
-                type="checkbox"  
-                id="invalidCheck"
-                onclick="completeTask(${task.id})">
-                <li>${task.task_str}</li>`
-                );
-        }
-    };
-});
-
-
-
-// $('li.form-check-input').on('click', () => {
-
-//     alert('HEY!')
 
 // });
