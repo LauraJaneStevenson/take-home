9a200dd548a080e3e3126de443a5da4911ec3039
vagrant <vagrant@vagrant.vm>
2019-12-14 20:00:37 +0000

    checkboxes

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -3,13 +3,24 @@ $.get('/all_tasks.json',(res)=>{
     for(const task of res){
         if(task.complete){
             $('ul.tasks').append(
-                `<li class='done'>${task.task_str}</li>`
+                `<input class="form-check-input" type="checkbox" value="" id="invalidCheck">
+                <li class='done'>${task.task_str}</li>`
                 );
         }else{
             $('ul.tasks').append(
-                `<li>${task.task_str}</li>`
+                `<input class="form-check-input" type="checkbox" value="" id="invalidCheck">
+                <li>${task.task_str}</li>`
                 );
         }
     }
 });
+
+// <div class="form-check">
+//       <input class="form-check-input" type="checkbox" value="" id="invalidCheck">
+//       <label class="form-check-label" for="invalidCheck">
+//         Agree to terms and conditions
+//       </label>
+//       <div class="invalid-feedback">
+//         You must agree before submitting.
+//       </div>
 
